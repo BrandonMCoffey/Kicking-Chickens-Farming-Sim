0fa4214bee305138c92ee26dc08838b162dc2d4d
Brandon Coffey <bmc939@gmail.com>
2023-04-07 18:45:16 -0500

    Desktop AR Simulator Controller

---
+++ Assets/Scripts/Game/DesktopArCameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesktopArCameraMovement : MonoBehaviour
+{
+	[SerializeField] private float _moveSpeed = 2;
+	[SerializeField] private float _lookSpeed = 20;
+	[SerializeField] private Vector2 _pitchClamp = new Vector2(-15, 60);
+	[SerializeField] private Transform _cameraTransform;
+	
+	[SerializeField, ReadOnly] private float _pitch;
+	
+	private void Awake()
+	{
+		_pitch = _cameraTransform.localEulerAngles.x;
+	}
+	
+	private void Update()
+	{
+		if (InputManager.HoldingScreen)
+		{
+			// Look
+			var look = InputManager.LookDir * _lookSpeed * Time.deltaTime;
+			transform.Rotate(new Vector3(0, look.x, 0));
+			_pitch -= look.y;
+			_pitch = Mathf.Clamp(_pitch, _pitchClamp.x, _pitchClamp.y);
+			_cameraTransform.localRotation = Quaternion.Euler(new Vector3(_pitch, 0, 0));
+			
+		}
+		// Move
+		var move = InputManager.MoveDir * _moveSpeed * Time.deltaTime;
+		transform.position += move.y * transform.forward + move.x * transform.right;
+	}
+}
---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+	[SerializeField] private float _maxHoldForTap = 0.2f;
+	[SerializeField] private bool _debug;
+	[SerializeField] private bool _debugMovement;
+	
+	private bool _tapping;
+	private float _tapDuration;
+	
+	public static Vector2 MoveDir { get; private set; }
+	public static Vector2 LookDir { get; private set; }
+	public static bool HoldingScreen;
+	public static System.Action TapScreen = delegate { };
+	
+	private void Update()
+	{
+		if (_tapping)
+		{
+			_tapDuration += Time.deltaTime;
+			HoldingScreen = _tapDuration > _maxHoldForTap;
+		}
+	}
+	
+	private void OnMove(InputValue value)
+	{
+		MoveDir = value.Get<Vector2>();
+		Log("Move: " + MoveDir, true);
+	}
+	
+	private void OnLook(InputValue value)
+	{
+		LookDir = value.Get<Vector2>();
+		Log("Look: " + LookDir, true);
+	}
+	
+	private void OnFire(InputValue value)
+	{
+		if (value.isPressed)
+		{
+			_tapping = true;
+			Log("Begin Hold");
+		}
+		else
+		{
+			if (_tapDuration <= _maxHoldForTap)
+			{
+				Log("Tap");
+				TapScreen?.Invoke();
+			}
+			else Log("End Hold");
+			_tapping = false;
+			HoldingScreen = false;
+		}
+		_tapDuration = 0;
+	}
+	
+	private void Log(string message, bool movement = false)
+	{
+		#if UNITY_EDITOR
+		if (movement && _debugMovement || !movement && _debug) Debug.Log(message, gameObject);
+		#endif
+	}
+}

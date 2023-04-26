2172321750e5513bfe7c70f3cbc66d31a37a5af4
Haven Gibbs <oilpastel701@gmail.com>
2023-04-26 18:25:22 -0500

    basic button press anim

--- Assets/Scripts/UI Animations/ButtonPress.cs
+++ Assets/Scripts/UI Animations/ButtonPress.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ButtonPress : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject panel;
+    [SerializeField] private float scale;
+    [SerializeField] private float duration;
+
+    public void ClickFeedback()
     {
+        LeanTween.scale(this.gameObject, new Vector2(scale, scale), duration*.5f).setEaseOutBack();
+        LeanTween.scale(this.gameObject, new Vector2(1, 1), duration*.55f).setEaseOutExpo().setDelay(duration*.45f);
+        StartCoroutine(WaitForAnim(duration));
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void WaitCoroutine()
+    {
+        StartCoroutine(WaitForAnim(duration));
+    }
+
+    public IEnumerator WaitForAnim(float duration)
     {
         
+        yield return new WaitForSeconds(duration);
+        
+        if(panel != null)
+            panel.SetActive(!panel.activeSelf);
+
+        
     }
+
 }

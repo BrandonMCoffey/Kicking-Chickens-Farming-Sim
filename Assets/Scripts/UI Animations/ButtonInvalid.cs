16d838a66e7ed9f26b18b0e7ee24740ccb32cb32
Haven Gibbs <oilpastel701@gmail.com>
2023-04-26 23:20:05 -0500

    some animations

--- Assets/Scripts/UI Animations/ButtonInvalid.cs
+++ Assets/Scripts/UI Animations/ButtonInvalid.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Economy;
 
 public class ButtonInvalid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int cost;
+
+    [SerializeField] private float shakeAmount;
+    [SerializeField] private float vertShake;
+    [SerializeField] private float duration;
+    [SerializeField] private GameObject denyText;
+
+    [SerializeField] private float growAmount;
+    [SerializeField] private float growDuration;
+
+    [SerializeField] private EconomyManager eco;
+
+    public void ChackIfValidPurchase()
     {
-        
+        if (!eco.CanAfford(cost))
+            ShakeButton();
+        else if (eco.CanAfford(cost))
+            PurchaseAnim();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShakeButton()
     {
-        
+        float posOrigX = this.transform.position.x;
+        float posOrigY = this.transform.position.y;
+
+        LeanTween.moveX(this.gameObject, posOrigX + shakeAmount, duration * .25f).setEaseInQuint();
+        LeanTween.moveX(this.gameObject, posOrigX-shakeAmount, duration * .5f).setEaseInOutQuint().setDelay(duration *.25f);
+        LeanTween.moveX(this.gameObject, posOrigX + shakeAmount*.5f, duration * .25f).setEaseOutQuint().setDelay(duration *.75f);
+
+        LeanTween.moveY(this.gameObject, posOrigY + vertShake, duration * .25f).setEaseInQuint();
+        LeanTween.moveY(this.gameObject, posOrigY -vertShake, duration * .5f).setEaseInOutQuint().setDelay(duration * .25f);
+        LeanTween.moveY(this.gameObject, posOrigY + vertShake * .5f, duration * .25f).setEaseOutQuint().setDelay(duration * .75f);
+
+        //LeanTween.moveLocalY(this.gameObject, vertShake, duration / 2);
+        //LeanTween.moveLocalY(this.gameObject, -vertShake, duration / 2).setDelay(duration/2);
+
+        if (denyText != null)
+            denyText.SetActive(true);
+        StartCoroutine(WaitForAnim(duration));
+
+        //Debug.Log("shake button");
     }
+
+    private IEnumerator WaitForAnim(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        if (denyText != null)
+            denyText.SetActive(false);
+    }
+
+    public void PurchaseAnim()
+    {
+        Debug.Log("valid purchase");
+    }
+
 }
